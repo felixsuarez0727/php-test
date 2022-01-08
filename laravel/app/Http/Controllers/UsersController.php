0ff4af0cb868125333f518c8902b5c1d0c60a337
felixsuarez0727 <felixdavidsuarezbonilla@gmail.com>
2022-01-08 08:59:22 -0600

    API endpoints implemented using PHP framework Laravel 8

---
+++ laravel/app/Http/Controllers/UsersController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\users;
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function add_user(Request $req)
+    {
+        $user = new Users;
+        $user->name = $req->name;
+        $user->last_name = $req->last_name;
+        $user->email = $req->email;
+        $user->password = $req->password;
+        $isSaved = $user->save();
+        if ($isSaved) {
+            return ["Result" => "Data has been saved"];
+        }
+        return ["Result" => "Operation Failed"];
+    }
+
+    public function user(Request $req)
+    {
+        $user = new Users;
+        $user->name = $req->name;
+        $user->last_name = $req->last_name;
+        $user->email = $req->email;
+        $user->password = $req->password;
+        $isSaved = $user->save();
+        if ($isSaved) {
+            return ["Result" => "Data has been saved"];
+        }
+        return ["Result" => "Operation Failed"];
+    }
+
+    public function get_user($id = null)
+    {
+        if($id == null){
+            return ["Result" => "User ID not specified"];
+        }
+        $isFetch = Users::find($id);
+
+        if (!$isFetch) {
+            return ["Result" => "User with this id does no exists!"];
+        }
+
+        return $isFetch;
+    }
+
+}
---
+++ solution.md
@@ -0,0 +1,4 @@
+# php-test
+PhP, MYSQL, frameworks test.
+https://vitux.com/ubuntu-xampp/
+http://elprogramadorjunior.blogspot.com/2015/04/instalar-xampp-en-ubuntu.html
